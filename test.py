b001b241e6abc5dd081d50b4f0d1755577af2cfe
LiSSk0 <105405909+LiSSk0@users.noreply.github.com>
2023-02-06 17:12:48 +0500

    TASK #3

--- test.py
+++ test.py
@@ -5,29 +5,57 @@ import pygame
 import requests
 
 SIZE = WIDTH, HEIGHT = 600, 450
-x, y = 133.794557, -28.694111
 spn_x, spn_y = 20, 20
+FPS = 5
 
-map_request = f"https://static-maps.yandex.ru/1.x/?ll={x},{y}&spn={spn_x},{spn_y}&l=sat"
 
-response = requests.get(map_request)
+def do_map(x, y):
+    map_request = f"https://static-maps.yandex.ru/1.x/?ll={x},{y}&spn={spn_x},{spn_y}&l=sat"
 
-if not response:
-    print("Ошибка выполнения запроса:")
-    print(map_request)
-    print("Http статус:", response.status_code, "(", response.reason, ")")
-    sys.exit(1)
+    response = requests.get(map_request)
+    if not response:
+        print("Ошибка выполнения запроса:")
+        print(map_request)
+        print("Http статус:", response.status_code, "(", response.reason, ")")
+        sys.exit(1)
 
-map_file = "map.png"
-with open(map_file, "wb") as file:
-    file.write(response.content)
+    map_file = "map.png"
+    with open(map_file, "wb") as file:
+        file.write(response.content)
 
-pygame.init()
-screen = pygame.display.set_mode(SIZE)
-screen.blit(pygame.image.load(map_file), (0, 0))
-pygame.display.flip()
-while pygame.event.wait().type != pygame.QUIT:
-    pass
-pygame.quit()
+    return map_file
 
-os.remove(map_file)
+
+def main():
+    x, y = 133.794557, -28.694111
+
+    pygame.init()
+    screen = pygame.display.set_mode(SIZE)
+    map_file = do_map(x, y)
+
+    clock = pygame.time.Clock()
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    x -= 1
+                if event.key == pygame.K_RIGHT:
+                    x += 1
+                if event.key == pygame.K_UP:
+                    y += 1
+                if event.key == pygame.K_DOWN:
+                    y -= 1
+                map_file = do_map(x, y)
+
+        clock.tick(FPS)
+        screen.blit(pygame.image.load(map_file), (0, 0))
+        pygame.display.flip()
+    pygame.quit()
+    os.remove(map_file)
+
+
+if __name__ == '__main__':
+    main()
